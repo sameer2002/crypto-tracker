5ccebadb256de3b6749745f33d54971a22e4ae10
sameer2002 <sameersagar2001@gmail.com>
2023-07-29 20:41:19 +0530

    project-f2-contest3-up

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function showPage(index) {
+  const pages = document.querySelectorAll('.pages .page');
+  const tabs = document.querySelectorAll('.tabs .tab');
+
+  // Hide all pages and remove the 'active' class from tabs
+  pages.forEach(page => page.classList.remove('active'));
+  tabs.forEach(tab => tab.classList.remove('active'));
+
+  // Show the selected page and set the 'active' class on the clicked tab
+  pages[index].classList.add('active');
+  tabs[index].classList.add('active');
+  if(tabs.ClassName==='active'){
+      tabs.style.backgroundColor = "red";
+  }
+}
+
 let data = [];
 
 fetch(
